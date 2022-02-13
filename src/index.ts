7fbefb40f1b20a6373be31542a07258919a78634
nhomble <nhomble@terpmail.umd.edu>
2022-02-13 08:42:16 -0500

    add guessing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { solution } from "./wordle" 
-
+import { solution, testWord, GuessResult as WordleResult } from "./wordle"
+import { miss, reorder, correct } from "./fonts"
 export const wordleSolution = solution
+export const wordleTest = testWord
+export const fonts = {
+    "miss": miss,
+    "reorder": reorder,
+    "correct": correct
+};
--- src/wordle.ts
+++ src/wordle.ts
@@ -3,14 +3,62 @@ import { words } from "./words";
 const millisInDay = 864e5;
 const today = new Date()
 
-function solutionId(date: Date = today): number {
+function solutionId(date: Date): number {
   const start = new Date(2021, 5, 19, 0, 0, 0, 0);
   const diff = date.setHours(0, 0, 0, 0) - start.setHours(0, 0, 0, 0);
   return Math.round(diff / millisInDay);
 }
 
-export const solution = function (date: Date): string {
+export const solution = function (date: Date = today): string {
   const id = solutionId(date);
   const idx = id % words.length;
   return words[idx];
+};
+
+export type GuessResult = {
+  correct: number[];
+  reorder: number[];
+}
+
+export const testWord = function (guess: string, goal: string = null): GuessResult {
+  if (goal === null) {
+    goal = solution();
+  }
+  if ( !guess || guess.length > 5) {
+    return {
+      correct: [],
+      reorder: []
+    };
+  }
+
+  const freq = new Map();
+  for (var i = 0; i < goal.length; i++) {
+    var n = freq.get(goal.charAt(i));
+    n = n === undefined ? 1 : n + 1;
+    freq.set(goal.charAt(i), n);
+  }
+
+  const correct = [];
+  const reorder = [];
+  for (var i = 0; i < goal.length; i++) {
+    if (goal.charAt(i) === guess.charAt(i)) {
+      correct.push(i);
+      var n = freq.get(guess.charAt(i));
+      n -= 1;
+      freq.set(guess.charAt(i), n);
+    } else {
+      var n = freq.get(guess.charAt(i));
+      n = n === undefined ? 0 : n;
+      if (n > 0) {
+        reorder.push(i);
+      }
+      n -= 1;
+      freq.set(guess.charAt(i), n);
+    }
+  }
+
+  return {
+    correct: correct,
+    reorder: reorder
+  }
 };
