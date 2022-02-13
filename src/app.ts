9c24f0cc1aa5525d4f677a2e942360b469b15844
nhomble <nhomble@terpmail.umd.edu>
2022-02-13 10:20:55 -0500

    start heroku on port 5000

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { wordleSolution } from './wordle';
 
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 
 const getLocationsWithTimezones = (request: Request, response: Response, next: NextFunction) => {
