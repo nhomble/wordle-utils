08b0bca724117b9786f84a2d3f87ad37ac824ce0
nhomble <nhomble@terpmail.umd.edu>
2022-02-13 11:58:46 -0500

    msg groupme

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { wordleSolution } from './wordle';
+import { wordleTest, fonts, wordleSolution } from './wordle';
+import GroupMe from 'groupme';
+import { groupCollapsed } from 'console';
+import axios from 'axios';
+const groupmeClient = GroupMe.Stateless;
 
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 8080;
-
+const groupmeToken = process.env.G_TOKEN;
+const groupmeBot = process.env.G_ID;
 
 const routeSolution = (request: Request, response: Response, next: NextFunction) => {
     const data = {
@@ -16,9 +21,37 @@ const routeSolution = (request: Request, response: Response, next: NextFunction)
     response.status(200).json(data);
 };
 
-const routeMessage = (request: Request, response: Response, next: NextFunction) => {
-    console.log(request.body);
+const chatMessage = function (message: string) {
+    const res = axios.post('hhttps://api.groupme.com/v3/bots/post', { text: message, bot_id: groupmeBot }, {
+        headers: {
+            // 'application/json' is the modern content-type for JSON, but some
+            // older servers may use 'text/json'.
+            // See: http://bit.ly/text-json
+            'Authorization': groupmeToken
+        }
+    });
+}
 
+const routeMessage = (request: Request, response: Response, next: NextFunction) => {
+    const message = request.body.text;
+    const author = request.body.name;
+    const parts = message.split(" ");
+
+    if (parts[0] === "/wordle") {
+        const guess = parts[1];
+        const solution = wordleTest(guess);
+
+        const output = [fonts.miss, fonts.miss, fonts.miss, fonts.miss, fonts.miss];
+        for (var i = 0; i < solution.correct.length; i++) {
+            output[solution.correct[i]] = fonts.correct;
+        }
+        for (var i = 0; i < solution.reorder.length; i++) {
+            output[solution.reorder[i]] = fonts.reorder;
+        }
+        const res = guess + "\n" + output.join("");
+        chatMessage(res);
+        console.log(res);
+    }
     response.status(200).json({});
 };
 
